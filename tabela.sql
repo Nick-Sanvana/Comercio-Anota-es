253d5f90e35fb5a332f97009a85816d50e5837d2
Nick-Sanvana <nickdossantos68@gmail.com>
2026-08-27 09:28:40 -0300

    atualização

--- tabela.sql
+++ tabela.sql
@@ -57,3 +57,54 @@ REFERENCES produtos(id)
 on delete restrict
 
 )
+
+insert into categorias (nome) VALUES
+('Perifericos'),
+('Monitores'),
+('Hardware')
+
+insert into clientes(nome,email,cpf)VALUES
+('Victor', 'vitor@teste', '00011122233'),
+('Matheus','matheus@teste','0011133322')
+('Noah Navarro', 'Noah.Navarro@email.com', '12345678901')
+
+insert into produtos (categorias_id , nome, preco, quantidade_estoque) VALUES
+(1, 'Teclado LogiTech' , 120.00,100),
+(1, 'MousePad Philips' , 39.99, 500),
+(2, 'Monitor Philips 24p 244hz' , 899.90,10),
+(2, 'Monitor AOC 27p 75hz' , 999.99,150),
+(2, 'Monitor Mancer 17p 240hz', 500.00, 600),
+(3, 'Placa Mae Asus A520' , 450.90,300),
+(3, 'Memoria Ram DDR4 Redragon 8GB' , 500,700),
+(3, 'SSD 1TB Mancer' , 800.50,49)
+
+insert into pedidos (clientes_id, status ) VALUES
+(1, 'PAGO'),
+(1, 'ENVIADO'),
+(2, 'CANCELADO'),
+(2, 'PAGO'),
+(2, 'PENDENTE'),
+(3, 'CANCELADO'),
+(3, 'PENDENTE'),
+(3, 'ENVIADO')
+
+insert into itens_pedido (pedidos_id, produtos_id ,quantidade, preco_unitario ) VALUES
+(1,1,3,200),
+(15, 2, 3, 1500),
+(16, 3, 4, 1000),
+(17, 1, 3, 750.50),
+(18, 2, 3, 597),
+(19, 3, 3, 300),
+(20, 1, 3, 497),
+(21, 2, 3, 111.99),
+(22, 3, 3, 601)
+
+select
+	p.nome as produto,
+	c.nome as categorias,
+	p.preco,
+	p.quantidade_estoque
+	
+	from produtos p
+	join categorias c on c.id = p.categorias_id
+	order by p.preco desc;
